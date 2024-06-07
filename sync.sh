9ac81ee30870d4a412d5e26696a4b77c1893cdbc
alexdr3437 <alexanderdingwall5398@gmail.com>
2024-06-07 09:27:21 -0400

    move to .config and use stow

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+# copies your local config into this repo
+
+cp -r ~/.config/nvim/** ./nvim/
+cp -r ~/.config/tmux/** ./tmux/
+cp -r ~/.config/alacritty/** ./alacritty/
+cp -r ~/.config/i3/** ./i3/
+cp -r ~/.config/i3blocks/** ./i3blocks/
+
