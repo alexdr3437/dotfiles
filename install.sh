992a4fae1c82bbbb3e5846ae29b8b509e2271c9a
alexdr3437 <alexanderdingwall5398@gmail.com>
2024-08-24 10:47:19 -0400

    install neovim

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 # this script copies all the files to where they belong and installs any necessary dependencies
 
-sudo apt install compton i3blocks stow tmux zsh
+pwd=$(pwd)
+
+sudo apt install git compton i3blocks stow tmux zsh cmake
+
+cd 
+git clone https://github.com/neovim/neovim.git
+cd neovim
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+
+cd $pwd
 
 # set zsh as default shell
 chsh -s $(which zsh)
@@ -17,6 +27,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # install dotfiles
+cd $pwd
 stow .
 
 # betterlockscreen
@@ -25,9 +36,7 @@ wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/in
 # i3lock-color
 sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
 
-pwd=$(pwd)
-
-cd ..
+cd 
 git clone https://github.com/Raymo111/i3lock-color.git
 cd i3lock-color
 ./install-i3lock-color.sh
