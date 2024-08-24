6b3af63cce1916c8c77317d87595fe9c207f5343
alexdr3437 <alexanderdingwall5398@gmail.com>
2024-08-24 14:00:44 -0400

    remove

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-# copies your local config into this repo
-
-cp -r ~/.config/nvim/** ./nvim/
-cp -r ~/.config/tmux/** ./tmux/
-cp -r ~/.config/alacritty/** ./alacritty/
-cp -r ~/.config/i3/** ./i3/
-cp -r ~/.config/i3blocks/** ./i3blocks/
-
