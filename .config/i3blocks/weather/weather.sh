227f37c420d6b514396e9d45890fbc45ac0dbd0a
alexdr3437 <alexanderdingwall5398@gmail.com>
2025-06-17 07:40:34 -0400

    remove unneeded configs for nixos

--- .config/i3blocks/weather/weather.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-weather=($(curl --silent http://api.openweathermap.org/data/2.5/weather/\?id\=5969785\&units\=metric\&appid\=aa7a65d9aed0d5542e1c64fa1a23611d | jq -r '[(.main.temp|round), (.main.temp_max|round), (.weather[0].description)] | join(" ")'))
-echo "${weather[@]:2} ${weather[0]}°C | ${weather[1]}°C"
-
