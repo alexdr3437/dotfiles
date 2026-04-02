883019ba5a1472eea69d2a9338db3147802c37bc
alexdr3437 <alexanderdingwall5398@gmail.com>
2026-04-02 10:16:07 -0400

    update nvidia kernel packages

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -13,6 +13,6 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 }
