12a1329371c481e3c36588d24f86f9a4af446010
alexdr3437 <alexanderdingwall5398@gmail.com>
2026-07-22 16:51:50 -0400

    direnv

--- home/editor.nix
+++ home/editor.nix
@@ -1,5 +1,7 @@
 { ... }:
 {
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
   programs.zed-editor = {
     enable = true;
     extensions = [
@@ -13,6 +15,10 @@
         mode = "system";
         dark = "One Dark";
         light = "One Light";
+      };
+      preview_tabs = {
+        enabled = true;
+        enable_preview_from_file_finder = true;
       };
       hour_format = "hour24";
       vim_mode = true;
